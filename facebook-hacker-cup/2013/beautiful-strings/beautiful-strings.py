336325ea301ea1526ef1b2f8454d45fad2dcc1e4
aofleejay <aofleejay@hotmail.com>
2018-05-14 00:09:37 +0700

    Refactoring code of beautiful strings

--- facebook-hacker-cup/2013/beautiful-strings/beautiful-strings.py
+++ facebook-hacker-cup/2013/beautiful-strings/beautiful-strings.py
@@ -1,20 +1,15 @@
-import re
-import string
-import functools
+from collections import Counter
 
 f = open('beautiful-strings.in', 'r')
-N = f.readline()
+m = f.readline()
 sentences = f.readlines()
-alphabetList = list(string.ascii_lowercase)
+
 for index, sentence in enumerate(sentences):
-  unique = []
-  onlyAlphabetSentence = re.sub(r"[^A-Za-z]+", '', sentence.lower())
-  for letter in alphabetList:
-    if onlyAlphabetSentence.count(letter):
-      unique.append(onlyAlphabetSentence.count(letter))
-  unique.sort(reverse=True)
-  count = 26
-  su = 0
-  for i, v in enumerate(unique):
-    su += v * (26 - i)
-  print("Case #{}: {}".format(index + 1, su))
+  sentence = ''.join(letter for letter in sentence if letter.isalpha()).lower()
+  sentenceDictionary = Counter(sentence)
+  countAlphabet = list(sentenceDictionary.values())
+  countAlphabet.sort()
+  countAlphabet.reverse()
+  countWithMultiply = zip(range(26, 0, -1), countAlphabet)
+  result = sum(count * multiply for count, multiply in countWithMultiply)
+  print("Case #{}: {}".format(index + 1, result))

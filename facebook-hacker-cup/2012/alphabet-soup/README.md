8d5b4aa724f8806f2e432394138b13bb918f991d
aofleejay <aofleejay@hotmail.com>
2018-05-15 20:32:48 +0700

    Solved alphabet soup

---
+++ facebook-hacker-cup/2012/alphabet-soup/README.md
@@ -0,0 +1,3 @@
+# Alphabet Soup #
+
+Find frequency of each letter in 'HACKERUP' and sort in ascending (Divide frequency of letter 'C' by 2). If we have all eight letter (HACKERUP) the first index of a sorted array will be the answer.
---
+++ facebook-hacker-cup/2012/alphabet-soup/alphabet-soup.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+f = open('alphabet-soup.in', 'r')
+T = f.readline()
+sentences = f.readlines()
+
+for index, sentence in enumerate(sentences):
+  sentence = ''.join([letter for letter in sentence if letter in 'HACKERCUP'])
+  frequency = Counter(sentence)
+  frequency['C'] = frequency['C'] / 2
+  values = list(frequency.values())
+  if (len(values) != 8): 
+    print("Case #{}: 0".format(index + 1))
+  else:
+    values.sort()
+    print("Case #{}: {}".format(index + 1, values[0]))

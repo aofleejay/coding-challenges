5a1df9f5616eb53a71e07c9a72319b8a0e2a95ca
aofleejay <aofleejay@hotmail.com>
2020-04-14 04:42:53 +0700

    :recycle: Remove unnecessary loop.

--- leetcode/two-sum/solution-2.js
+++ leetcode/two-sum/solution-2.js
@@ -3,16 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
   const t = {}
-  for (i = 0; i < nums.length; i++) {
-    t[nums[i]] = i
-  }
-
   for (i = 0; i < nums.length; i++) {
     a = target - nums[i]
     if (t[a] !== undefined && t[a] !== i) {
       return [i, t[a]]
     }
+    t[nums[i]] = i
   }
 }
